5d544ec6942ee3a37cd2b6bd665194037414802c
Murray Tinker <2tinkers@gmail.com>
2026-05-23 09:14:43 +1000

    fix: invert panorama background sphere vertically to display sky at the top

--- clean-court/src/App.tsx
+++ clean-court/src/App.tsx
@@ -238,7 +238,7 @@ function PanoramaBackground() {
   const texture = useTexture('/parkland_panorama.png');
   texture.colorSpace = THREE.SRGBColorSpace;
   return (
-    <mesh rotation={[0, -Math.PI / 2, 0]}>
+    <mesh rotation={[0, -Math.PI / 2, 0]} scale={[1, -1, 1]}>
       <sphereGeometry args={[1000, 60, 40]} />
       <meshBasicMaterial map={texture} side={THREE.BackSide} toneMapped={false} fog={false} />
     </mesh>
